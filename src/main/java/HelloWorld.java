408ac4fc98d25068bd481c74ad9858691cdf53b5
Sarai De Leon <saraideleon@live.com>
2019-07-19 09:54:04 -0500

    changes

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,5 +1,12 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
+
+    //Add a method that concatenates multiple strings. Overload this method.
+        String word1 = "Hello";
+        String word2 = "Universe";
+        System.out.println(word1 + word2);
+        word1 = "goodbye";
+        System.out.println(word1 + word2);
     }
 }
